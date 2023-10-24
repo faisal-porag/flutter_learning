ae6e2e3c3da30ea81e659322499239b3ad8ea0e1
porag_faisal123@hotmail.com <porag_faisal123@hotmail.com>
2023-10-24 15:58:07 +0600

    add new item design added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,33 +14,83 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 50,
-                      bottom: 20,
-                    ),
-                    child: Text(
-                      'All ToDos',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,  
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,  
+                          ),
+                        ),
                       ),
-                    ),
+                      for (ToDO todoo in todoList)
+                        TodoItem(todo: todoo,),
+                     
+                  ],),
+                )
+            ]),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                  bottom: 20, 
+                  right: 20, 
+                  left: 20,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5,),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: const [BoxShadow(
+                    color: Colors.grey, 
+                    offset: Offset(0.0, 0.0),
+                    blurRadius: 10.0,
+                    spreadRadius: 0.0,
+                  ),],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Add new todo item",
+                    border: InputBorder.none,
                   ),
-                  for (ToDO todoo in todoList)
-                    TodoItem(todo: todoo,),
-                 
-              ],),
-            )
-        ]),
+                ),
+              ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  child: Text("+", style: TextStyle(fontSize: 40, color: Colors.white),),
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    primary: tdBlue,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  ),
+                ),
+              )
+            ]),
+          )
+        ],
       ),
     );
 
